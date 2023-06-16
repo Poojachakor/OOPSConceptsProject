0cc682870842b9ee3a278fccd25170808472897f
Poojachakor <chakorpooja07@gmail.com>
2023-06-16 19:27:44 +0530

    Add: OOPSConcepts

---
+++ OOPSConcepts/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConcepts
+{
+     public class Car
+     {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+
+        
+        public void Start()
+        {
+            Console.WriteLine("The car is starting.");
+        }
+
+      
+        public void Stop()
+        {
+            Console.WriteLine("The car is stopping.");
+        }
+     }
+   
+       
+}
---
+++ OOPSConcepts/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace OOPSConcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            
+            myCar.Model = "Thar";
+            myCar.Color = "Black";
+            myCar.Year = 2025;
+
+            
+            Console.WriteLine("Car Details:");
+            Console.WriteLine("Model: " + myCar.Model);
+            Console.WriteLine("Color: " + myCar.Color);
+            Console.WriteLine("Year: " + myCar.Year);
+
+            myCar.Start();
+            myCar.Stop();
+
+            Console.ReadLine();
+        }
+    }
+    
+}
